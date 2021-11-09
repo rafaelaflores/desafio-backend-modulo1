9dd231963e60622d5801c9a3323d6a03ba1ad5b7
Rafaela Flores <rafaela.jardim@southsystem.com.br>
2021-11-09 17:22:09 -0300

    Tentando utilizar spring boot

--- src/main/java/br/com/southsystem/loja/App.java
+++
@@ -1,9 +0,0 @@
-package br.com.southsystem.loja;
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Olá mundo!" );
-        System.out.println( "Usando o Intellij :)" );
-    }
-}
