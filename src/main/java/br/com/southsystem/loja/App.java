1fa747837b67636ca2a9bb27324cc0a0cbca9ca5
Rafaela Flores <rafaela.jardim@southsystem.com.br>
2021-11-08 21:03:44 -0300

    Estruturando o projeto

---
+++ src/main/java/br/com/southsystem/loja/App.java
@@ -0,0 +1,13 @@
+package br.com.southsystem.loja;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
