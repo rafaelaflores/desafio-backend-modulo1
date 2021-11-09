49ef122af226f4cf56b07aa62b5f64f18f2e84b1
Rafaela Flores <rafaela.jardim@southsystem.com.br>
2021-11-08 21:26:23 -0300

    Alterando conteúdo app.java

--- src/main/java/br/com/southsystem/loja/App.java
+++ src/main/java/br/com/southsystem/loja/App.java
@@ -1,13 +1,9 @@
 package br.com.southsystem.loja;
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Olá mundo!" );
+        System.out.println( "Usando o Intellij :)" );
     }
 }
